b709b6ebee025559352cc32931aef59ba1b99183
Bipin Thapa <yukichin638@gmail.com>
2024-12-23 21:32:17 +0545

    Day 65

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,9 +4,15 @@ function Nav(){
   return(
     <>
       <nav>
-      
       <a href="#">Mero Note</a>
-      <button className="nav-button"><i className="fa-solid fa-ellipsis"></i></button>
+      <div className='nav-button-holder'>
+        <button className="nav-button">
+          <i className="fa-solid fa-ellipsis"></i> 
+        </button>
+        <div className="nav-button-option">
+          Login features are to be added soon  
+        </div> 
+      </div>
       </nav>
     </>
   );
